afdca712440a9549ddb0e65bda3af6199e04daa8
Devilsbkssks <119072774+Devilsbkssks@users.noreply.github.com>
2023-01-04 16:42:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Devilsbkssks/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/Devilsbkssks/URL-Shortener-V.git /URL-Shortener-V2
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /URL-Shortener-V
 fi
-cd /URL-Shortener-V2
+cd /URL-Shortener-V
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m main
